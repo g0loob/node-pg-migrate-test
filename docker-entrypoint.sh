367e865c4cec38085d3028c6a9f80e55f13becbe
Stefan Golubovic <stefan.golubovic@gmx.com>
2020-03-25 21:20:48 +0100

    add some comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# could't install postgresql-client
+# https://serverfault.com/a/819511
 for count in {1..100}; do
     echo "Pinging database attempt "${count}
     if  $(nc -z ${DB_HOST} ${DB_PORT}) ; then
